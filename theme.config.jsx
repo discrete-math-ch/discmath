18d62121af247d0c18f0541d1a6ce804edd318aa
Tobotis <to.steinbrecher@gmail.com>
2024-09-08 22:33:53 +0200

    footer

--- theme.config.jsx
+++ theme.config.jsx
@@ -6,5 +6,13 @@ export default {
     },
     primaryHue: {dark: 73, light:73 },
     primarySaturation: {dark: 60, light:60},
-
+    footer: {
+        text: (
+          <div className="flex w-full flex-col items-center sm:items-start">
+            <p className="mt-6 text-xs">
+              © {new Date().getFullYear()} some student TAs
+            </p>
+          </div>
+        )
+      },
   }
