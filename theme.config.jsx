619c06b08bfd7f29b75e51cfa0e509bfe87acbfd
Tobotis <to.steinbrecher@gmail.com>
2024-09-08 22:23:48 +0200

    init

---
+++ theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+     docsRepositoryBase: 'https://github.com/tobotis/discmath',
+    logo: <span>DiscMath</span>,
+    project: {
+      link: 'https://github.com/tobotis/discmath'
+    },
+    primaryHue: {dark: 73, light:73 },
+    primarySaturation: {dark: 60, light:60},
+
+  }
