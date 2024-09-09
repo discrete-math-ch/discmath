8bef7da3222b32536b6abef3796ff2e02bb92ebd
Tobotis <to.steinbrecher@gmail.com>
2024-09-09 16:24:19 +0200

    test

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withNextra = require('nextra')({
     latex: true
   })
    
-module.exports = withNextra()
-// If you have other Next.js configurations, you can pass them as the parameter:
-// module.exports = withNextra({ /* other next.js config */ })
+  module.exports = {
+    ...withNextra(),
+    images: {
+      unoptimized: true,
+    },
+  };
