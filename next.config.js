1bd482272ad0f8f5533814211219295f07a1955f
MingXu <91617361+M1ngXU@users.noreply.github.com>
2024-09-18 15:15:29 +0000

    fix config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 const withNextra = require('nextra')({
-  output: 'export',
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
   latex: true
 })
- 
-module.exports = withNextra()
+
+module.exports = withNextra({
+    output: 'export',
+    images: {
+        unoptimized: true
+    }
+})
