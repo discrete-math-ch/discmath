35653b9adb86ad258ddcc88e5c346d5e30c3dab8
MingXu <91617361+M1ngXU@users.noreply.github.com>
2024-09-20 19:48:57 +0000

    added useful links/favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
  
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+          <Head>
+            <title>Discrete Mathematics @ ETHZ</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <Component {...pageProps} />
+      </>
+  )
 }
