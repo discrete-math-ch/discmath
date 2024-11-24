0da3fb282cf043d3a0c1b768075e311f266589e7
Tobias Steinbrecher <to.steinbrecher@gmail.com>
2024-11-24 22:00:14 +0100

    polynomial remainder practice

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -3,12 +3,14 @@ import React from "react";
 interface ButtonProps {
   href: string;
   children: React.ReactNode;
+  onClick?: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ href, children }) => {
+const Button: React.FC<ButtonProps> = ({ href, children, onClick }) => {
   return (
     <a
       href={href}
+      onClick={onClick}
       className="inline-block px-5 py-3 rounded-lg bg-secondary text-background shadow hover:bg-secondary-dark transition-colors"
     >
       {children}
